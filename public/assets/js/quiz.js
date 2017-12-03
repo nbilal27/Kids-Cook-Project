2df546db5c2c2aa8d3a6e1763c334ca7732270f8
Barrett <Aliciawyse@users.noreply.github.com>
2017-12-03 09:40:55 -0500

    trying to display all quiz questions at once

--- public/assets/js/quiz.js
+++ public/assets/js/quiz.js
@@ -5,6 +5,7 @@
 var displayQuestion = 0;
 
 //use an object to grade how well user does in each category. Highest score for each category is 5.
+
 var categories = {
     "vegetable_score": 0,
     "fruit_score": 0,
@@ -50,116 +51,202 @@ var myListofQuestions = [
         choices: ["No", "Yes"],
         addPoints: [1,0],
         category: "protein_score"
-    }/*,
+    },
 
     {
         question: "Is Your child allergic to any of the following?",
         choices: ["Peanuts", "egg", "soy", "milk", "fish", "shellfish", "wheat/gluten"],
         addPoints: [1,1,1,1,1,1,1],
         category: "allergies"
-    }*/
+    }
     ];
 
-showQuestion();
 
-function showQuestion() {
+//show  quiz questions
 
+var panel = $("#quiz-area");
 
-    myQuestion = "";
+for (var i = 0; i < myListofQuestions.length; i++) {
 
-    myQuestion = myQuestion + "<p class='bold center_q'>" + myListofQuestions[displayQuestion].question + "</p>";
+    panel.append("<h2>" + myListofQuestions[i].question + "</h2>");
 
-    myListofQuestions[displayQuestion].choices.forEach(function(choice) {
-        myQuestion += "<input type='radio' id='" + choice + "' name='" + displayQuestion + "' value='" + choice + "' + class='selectable'><label for='" + choice + "' class='option-input'>" + choice + "</label><br>";
-    });
+    for (var j = 0; j < myListofQuestions[i].choices.length; j++) {
 
-    $("#quiz").html(myQuestion);
+        if(myListofQuestions[i].category === 'allergies'){
 
-    $(".selectable").on("click", function() {
+            panel.append("<input class='selectable' id=allergy" + [j] + " type='checkbox' name='question-" + i +
+                "' value='" + myListofQuestions[i].choices[j] + "''>" + myListofQuestions[i].choices[j]);
 
-       // var counter = 0;
-        var index = $("input").index(this);
+            //var index = $('input').attr('name');
+            //console.log(index);
 
-        //counter =+ ;
+           // grabAnswer(i,j);
 
-        processQuestion(index);
+        } else {
+            panel.append("<input class='selectable' id=" + [j] + " type='radio' name='question-" + i +
+                "' value='" + myListofQuestions[i].choices[j] + "''>" + myListofQuestions[i].choices[j]);
 
-    });
-}
-
-var results = [];
-var counter = 0;
-var quizResults = {};
-
-
-function processQuestion(idx) {
-
-    var pointsToAdd = myListofQuestions[displayQuestion].addPoints[idx];
-
-    var thisCategory = myListofQuestions[displayQuestion].category;
-
-    var currCategoryTotal = categories[thisCategory];
-
-    totalPoints += pointsToAdd;
-
-    categories[thisCategory] = currCategoryTotal + pointsToAdd;
-
-    if (displayQuestion < myListofQuestions.length - 1) {
-
-        results.push({
-            answers: myListofQuestions[displayQuestion].choices[idx],
-            question: myListofQuestions[displayQuestion].questionn
-        });
-
-        quizResults[myListofQuestions[displayQuestion].question] = myListofQuestions[displayQuestion].choices[idx];
-
-        counter++;
-        displayQuestion++;
-        showQuestion();
-
-    } else {
-
-        $("#quizWrapper").toggleClass("hidden");
-        $("#resultsWrapper").toggleClass("hidden");
-
-
-        console.log("Results object", results);
-        //console.log("Quiz Results with corresponding answers", quizResults);
-        console.log("Score per category", categories);
-
-       console.log("new categories", categories);
-
-        $.ajax("/results", {
-          type: "POST",
-          data: categories
-        }).then(
-          function(result) {
-            console.log(result)
-            $("#first").html(result[0].recipe_name)
-            $("#source1").html("<a href =" + result[0].source + ">" + result[0].source + "</a>")
-            $("#pic1").html("<img src=" + result[0].image + ">")
-
-            $("#second").html(result[1].recipe_name)
-            $("#source2").html("<a href =" + result[1].source + ">" + result[1].source + "</a>")
-            $("#pic2").html("<img src=" + result[1].image + ">")
-
-            $("#third").html(result[2].recipe_name)
-            $("#source3").html("<a href =" + result[2].source + ">" + result[2].source + "</a>")
-            $("#pic3").html("<img src=" + result[2].image + ">")
-
-            $("#fourth").html(result[3].recipe_name)
-            $("#source4").html("<a href =" + result[3].source + ">" + result[3].source + "</a>")
-            $("#pic4").html("<img src=" + result[3].image + ">")
-        }).done(console.log("this is done"));
-
-
-       // console.log("Total points (the user's grade for how well they feed their kid)", totalPoints);
-
-        var userEmail = window.localStorage.getItem("userEmail");
-        var userpw = window.localStorage.getItem("userpw")
-
-        console.log("userEmail", userEmail);
-        console.log("userpw", userpw);
+        }
     }
 
 }
+
+$("button").on("click", function() {
+
+    // var counter = 0;
+    //  var index = $("input").index(this);
+    //
+    //  var test = $('input').attr('id');
+    //
+    //  console.log(test);
+
+    grabAnswer();
+
+
+    // processQuestion(index);
+
+});
+
+//User clicks submit on index.html then the following function runs
+function grabAnswer() {
+
+    var results = [];
+    var counter = 0;
+    var quizResults = {};
+
+    //var isItChecked = $("input[name =" + i + "]:checked").val();
+
+    for (var i = 0; i < myListofQuestions.length; i ++) {
+
+        var isItChecked = $("input[name = question-" + i+"]:checked").val();
+
+        console.log(isItChecked);
+
+    }}
+
+
+
+
+
+
+//////////////////DON'T TOUCH CODE BELOW/////////////////////////
+
+// myQuestion = "";
+//
+// myListofQuestions.forEach(function(obj,idx){
+//
+//     myQuestion = myQuestion + "<p class='bold'>" + obj.question + "</p>";
+//
+//     obj.choices.forEach(function(choice) {
+//         myQuestion = myQuestion + "<input type='radio' name=" + idx + " value=" + choice + ">" + choice + "<br>";
+//
+//         console.log(myQuestion);
+//
+//     })
+//
+// });
+//
+// showQuestion();
+//
+// function showQuestion() {
+//
+//
+//     myQuestion = "";
+//
+//     myQuestion = myQuestion + "<p class='bold center_q'>" + myListofQuestions[displayQuestion].question + "</p>";
+//
+//     myListofQuestions[displayQuestion].choices.forEach(function(choice) {
+//         myQuestion += "<input type='radio' id='" + choice + "' name='" + displayQuestion + "' value='" + choice + "' + class='selectable'><label for='" + choice + "' class='option-input'>" + choice + "</label><br>";
+//     });
+//
+//     $("#quiz").html(myQuestion);
+//
+//     $(".selectable").on("click", function() {
+//
+//        // var counter = 0;
+//         var index = $("input").index(this);
+//
+//         //counter =+ ;
+//
+//         processQuestion(index);
+//
+//     });
+// }
+//
+// var results = [];
+// var counter = 0;
+// var quizResults = {};
+//
+//
+// function processQuestion(idx) {
+//
+//     var pointsToAdd = myListofQuestions[displayQuestion].addPoints[idx];
+//
+//     var thisCategory = myListofQuestions[displayQuestion].category;
+//
+//     var currCategoryTotal = categories[thisCategory];
+//
+//     totalPoints += pointsToAdd;
+//
+//     categories[thisCategory] = currCategoryTotal + pointsToAdd;
+//
+//     if (displayQuestion < myListofQuestions.length - 1) {
+//
+//         results.push({
+//             answers: myListofQuestions[displayQuestion].choices[idx],
+//             question: myListofQuestions[displayQuestion].questionn
+//         });
+//
+//         quizResults[myListofQuestions[displayQuestion].question] = myListofQuestions[displayQuestion].choices[idx];
+//
+//         counter++;
+//         displayQuestion++;
+//         showQuestion();
+//
+//     } else {
+//
+//         $("#quizWrapper").toggleClass("hidden");
+//         $("#resultsWrapper").toggleClass("hidden");
+//
+//
+//         console.log("Results object", results);
+//         //console.log("Quiz Results with corresponding answers", quizResults);
+//         console.log("Score per category", categories);
+//
+//        console.log("new categories", categories);
+//
+//         $.ajax("/results", {
+//           type: "POST",
+//           data: categories
+//         }).then(
+//           function(result) {
+//             console.log(result);
+//             $("#first").html(result[0].recipe_name);
+//             $("#source1").html("<a href =" + result[0].source + ">" + result[0].source + "</a>");
+//
+//             $("#pic1").html("<img src=" + result[0].image + ">");
+//
+//             $("#second").html(result[1].recipe_name);
+//             $("#source2").html("<a href =" + result[1].source + ">" + result[1].source + "</a>");
+//             $("#pic2").html("<img src=" + result[1].image + ">");
+//
+//             $("#third").html(result[2].recipe_name);
+//             $("#source3").html("<a href =" + result[2].source + ">" + result[2].source + "</a>")
+//             $("#pic3").html("<img src=" + result[2].image + ">");
+//
+//             $("#fourth").html(result[3].recipe_name);
+//             $("#source4").html("<a href =" + result[3].source + ">" + result[3].source + "</a>");
+//             $("#pic4").html("<img src=" + result[3].image + ">")
+//         }).done(console.log("this is done"));
+//
+//
+//        // console.log("Total points (the user's grade for how well they feed their kid)", totalPoints);
+//
+//         var userEmail = window.localStorage.getItem("userEmail");
+//         var userpw = window.localStorage.getItem("userpw")
+//
+//         console.log("userEmail", userEmail);
+//         console.log("userpw", userpw);
+//     }
+//
+// }
